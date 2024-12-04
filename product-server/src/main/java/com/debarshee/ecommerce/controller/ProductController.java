f12f2c357560188a948bded42c798df10b5031b1
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-12-04 08:35:53 +0530

    Added ProductController controller class

---
+++ product-server/src/main/java/com/debarshee/ecommerce/controller/ProductController.java
@@ -0,0 +1,47 @@
+package com.debarshee.ecommerce.controller;
+
+import com.debarshee.ecommerce.records.ProductPurchaseRequest;
+import com.debarshee.ecommerce.records.ProductPurchaseResponse;
+import com.debarshee.ecommerce.records.ProductRequest;
+import com.debarshee.ecommerce.records.ProductResponse;
+import com.debarshee.ecommerce.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService service;
+
+    @PostMapping
+    public ResponseEntity<Integer> createProduct(
+            @RequestBody @Valid ProductRequest request
+    ) {
+        return ResponseEntity.ok(service.createProduct(request));
+    }
+
+    @PostMapping("/purchase")
+    public ResponseEntity<List<ProductPurchaseResponse>> purchaseProducts(
+            @RequestBody List<ProductPurchaseRequest> request
+    ) {
+        return ResponseEntity.ok(service.purchaseProducts(request));
+    }
+
+    @GetMapping("/{product-id}")
+    public ResponseEntity<ProductResponse> findById(
+            @PathVariable("product-id") Integer productId
+    ) {
+        return ResponseEntity.ok(service.findById(productId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> findAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+}
