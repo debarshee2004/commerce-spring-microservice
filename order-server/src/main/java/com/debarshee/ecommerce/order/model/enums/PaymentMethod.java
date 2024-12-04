4ce8515cd13ac98191008ced228941b222cca331
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-12-04 10:44:27 +0530

    Created Order and OrderLine Models

---
+++ order-server/src/main/java/com/debarshee/ecommerce/order/model/enums/PaymentMethod.java
@@ -0,0 +1,9 @@
+package com.debarshee.ecommerce.order.model.enums;
+
+public enum PaymentMethod {
+    PAYPAL,
+    CREDIT_CARD,
+    VISA,
+    MASTER_CARD,
+    BITCOIN
+}
---
+++ order-server/src/main/java/com/debarshee/ecommerce/orderline/model/OrderLine.java
@@ -0,0 +1,24 @@
+package com.debarshee.ecommerce.orderline.model;
+
+import com.debarshee.ecommerce.order.model.Order;
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "customer_line")
+public class OrderLine {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    private Integer productId;
+    private double quantity;
+}
