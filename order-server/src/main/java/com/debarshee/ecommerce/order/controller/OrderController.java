70e0c7d6666b5da69fba6a7da815dfb371f22a67
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-12-04 11:36:06 +0530

    Created OrderController class

---
+++ order-server/src/main/java/com/debarshee/ecommerce/order/controller/OrderController.java
@@ -0,0 +1,38 @@
+package com.debarshee.ecommerce.order.controller;
+
+import com.debarshee.ecommerce.order.records.OrderRequest;
+import com.debarshee.ecommerce.order.records.OrderResponse;
+import com.debarshee.ecommerce.order.service.OrderService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/order")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService service;
+
+    @PostMapping
+    public ResponseEntity<Integer> createOrder(
+            @RequestBody @Valid OrderRequest request
+    ) {
+        return ResponseEntity.ok(this.service.createOrder(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderResponse>> findAll() {
+        return ResponseEntity.ok(this.service.findAllOrders());
+    }
+
+    @GetMapping("/{order-id}")
+    public ResponseEntity<OrderResponse> findById(
+            @PathVariable("order-id") Integer orderId
+    ) {
+        return ResponseEntity.ok(this.service.findById(orderId));
+    }
+}
