8202da9b150950ae91acc6c965009632efbf0ab4
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-12-04 19:19:17 +0530

    Created PaymentService and Mapper Classes

---
+++ payment-server/src/main/java/com/debarshee/ecommerce/service/PaymentMapper.java
@@ -0,0 +1,21 @@
+package com.debarshee.ecommerce.service;
+
+import com.debarshee.ecommerce.model.Payment;
+import com.debarshee.ecommerce.records.PaymentRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentMapper {
+
+    public Payment toPayment(PaymentRequest request) {
+        if (request == null) {
+            return null;
+        }
+        return Payment.builder()
+                .id(request.id())
+                .paymentMethod(request.paymentMethod())
+                .amount(request.amount())
+                .orderId(request.orderId())
+                .build();
+    }
+}
---
+++ payment-server/src/main/java/com/debarshee/ecommerce/service/PaymentService.java
@@ -0,0 +1,33 @@
+package com.debarshee.ecommerce.service;
+
+import com.debarshee.ecommerce.notifications.NotificationProducer;
+import com.debarshee.ecommerce.notifications.PaymentNotificationRequest;
+import com.debarshee.ecommerce.records.PaymentRequest;
+import com.debarshee.ecommerce.repository.PaymentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+
+    private final PaymentRepository repository;
+    private final PaymentMapper mapper;
+    private final NotificationProducer notificationProducer;
+
+    public Integer createPayment(PaymentRequest request) {
+        var payment = this.repository.save(this.mapper.toPayment(request));
+
+        this.notificationProducer.sendNotification(
+                new PaymentNotificationRequest(
+                        request.orderReference(),
+                        request.amount(),
+                        request.paymentMethod(),
+                        request.customer().firstname(),
+                        request.customer().lastname(),
+                        request.customer().email()
+                )
+        );
+        return payment.getId();
+    }
+}
