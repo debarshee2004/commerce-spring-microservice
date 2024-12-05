89a8608678c78539cf08cf8ef99f820f1a659a7f
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-12-05 09:11:13 +0530

    Created Payment Confirmation and its enums

---
+++ notification-server/src/main/java/com/debarshee/ecommerce/kafka/paymeny/PaymentConfirmation.java
@@ -0,0 +1,12 @@
+package com.debarshee.ecommerce.kafka.paymeny;
+
+import java.math.BigDecimal;
+
+public record PaymentConfirmation(
+        String orderReference,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        String customerFirstname,
+        String customerLastname,
+        String customerEmail
+) {}
